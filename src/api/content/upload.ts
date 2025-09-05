aa375944e28de191352bd93f8f30e69aa09412d1
ArchaicDeity <132263494+ArchaicDeity@users.noreply.github.com>
2025-09-05 14:59:27 +0200

    Remove duplicate file variable

--- src/api/content/upload.ts
+++ src/api/content/upload.ts
@@ -19,7 +19,6 @@ export default async function handler(req: Request, res: Response) {
     const blockId = parseInt(fields.blockId?.toString() ?? '', 10);
     const durationMinutes = fields.durationMinutes ? parseInt(fields.durationMinutes.toString(), 10) : null;
     const file = files.file as File | File[] | undefined;
-    const file = (files as Files & { file?: File | File[] }).file;
 
     
     if (!blockId || !file) {
